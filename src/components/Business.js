f419deb3b1d03201db38d89d8dedc8eb46433def
code-designphil <phil@design-pieper.com>
2023-07-28 17:49:25 +0200

    initial commit

---
+++ src/components/Business.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Business() {
+    const fakeBusiness = {
+        img: ""
+    };
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Business;
